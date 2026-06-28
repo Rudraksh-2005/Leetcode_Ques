0779f44a804aefa95a4d4e4188600342b931e0e6
Rudraksh Agrawal <144117271+Rudraksh-2005@users.noreply.github.com>
2026-06-28 17:02:09 +0530

    Time: 5 ms (68.38%) | Memory: 11.8 MB (65.88%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,14 +3,17 @@ public:
     int lengthOfLongestSubstring(string s) {
         int n=s.size();
         int maxi=0;
+        unordered_map<char,int>mpp;
+        int l=0;
         for(int i=0;i<n;i++){
-            vector<int>hash(256,0);
-            for(int j=i;j<n;j++){
-                if(hash[s[j]]!=0)break;
-                hash[s[j]]++;
-                maxi=max(maxi,j-i+1);
+            if(mpp.find(s[i])==0 || mpp[s[i]]<l){
+                mpp[s[i]]=i;
+                maxi=max(maxi,i-l+1);
             }
-
+            else{
+                l=mpp[s[i]]+1;
+                mpp[s[i]]=i;
+            }
         }
 
         return maxi;
